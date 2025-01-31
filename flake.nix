6525e741f3d4b7bf1c35168a0a56630aab82bd64
Jari Vetoniemi <jari.vetoniemi@cloudef.pw>
2025-01-31 18:59:47 +0900

    flake: target zig-0.13.0

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
     let
       zig-env = zig2nix.outputs.zig-env.${system};
       env = zig-env {
-        zig = zig2nix.outputs.packages.${system}.zig.master.bin;
+        zig = zig2nix.outputs.packages.${system}.zig."0.13.0".bin;
       };
     in with env.pkgs.lib; {
       # nix run
